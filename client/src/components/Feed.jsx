fbbd3e8924ef73191c7dffcdd63b000e0d0133a8
hrmtk <morgenzonne@zoho.com>
2022-01-14 15:23:21 +0900

    add feed

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -2,17 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams;
 
-  if(loading) return <Spinner message="hi" />
+  useEffect(() => {
+    setLoading(true);
+
+    if(categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+
+
+    } else {
+      client.fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    }
+  }, [categoryId])
+
+  if(loading) return <Spinner message="We are adding new ideas to your feed!" />
 
   return (
     <div>
-      feed
+      {pins && <MasonryLayout pins={pins} />}
     </div>
   )
 }
